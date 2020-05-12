816479b39a59f9f659174877d108ef93473cbf9e
Nahla Galal <nahlaglal@gmail.com>
2020-05-12 08:13:37 +0200

    Add Home page

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,13 +1,83 @@
-import React from 'react'
-import SwitchTheme from './SwitchTheme'
+import React from "react";
+import { Link } from "react-router-dom";
+import SwitchTheme from "./SwitchTheme";
+import profile from "../images/profile.jpg";
+import phoneIcon from "../images/phone.svg";
+import mailIcon from "../images/mail.svg";
+import data from '../util/data.js';
 
 const Home = () => {
   return (
-    <div>
+    <div className="Home">
+      {/** Switch language */}
       <SwitchTheme />
-      <section id="projects">Projects</section>
+      {/** About header */}
+      <header className="Home__header">
+        <div className="Home__header__image">
+          <img src={profile} alt="Nahla personal avatar" />
+        </div>
+        <h1>Hello,</h1>
+        <h2>I'm Nahla Galal</h2>
+        <p>
+          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quaerat
+          inventore a libero ut ratione fugiat, nesciunt illum quo corporis,
+          nisi numquam quidem explicabo! Explicabo ipsa soluta est praesentium,
+          quam fugit!
+        </p>
+        <div className="Home__header__contacts">
+          <div className="Home__header__contacts__icon">
+            <img src={phoneIcon} alt="Phone icon" />
+          </div>
+          <p>01097429922</p>
+          <div className="Home__header__contacts__icon">
+            <img src={mailIcon} alt="Email icon" />
+          </div>
+          <p>nahlaglal@gmail.com</p>
+          <div className="Home__header__contacts__icon">
+            <img src={phoneIcon} alt="Phone icon" />
+          </div>
+          <p>01097429922</p>
+        </div>
+      </header>
+      <main>
+        {/** Projects section */}
+        <section className="Home__projects">
+          <h2 className="Home__heading">My Projects</h2>
+          <span className="Home__heading-border"></span>
+          {data.slice(0, 3).map(project => (
+            <figure className="Home__projects__project" key={project.id}>
+              <div className="Home__projects__project__image">
+                <img src={project["main-image"]} alt={`${project.name} page screenshot`} />
+              </div>
+              <figcaption>
+                <Link to={`/project/${project.id}`}>{project.name}</Link>
+              </figcaption>
+            </figure>
+          ))}
+        </section>
+        {/** Contacts section */}
+        <section className="Home__contact">
+          <h2 className="Home__heading">Contact us</h2>
+          <span className="Home__heading-border"></span>
+          <form className="Home__contact__form">
+            <div className="Home__contact__form__field-input">
+              <input type="text" name="name" id="name" />
+              <label htmlFor="name">Your name</label>
+            </div>
+            <div className="Home__contact__form__field-input">
+              <input type="Email" name="email" id="email" />
+              <label htmlFor="email">Your email</label>
+            </div>
+            <div className="Home__contact__form__field-input">
+              <textarea name="message" id="message"></textarea>
+              <label htmlFor="message">Your message</label>
+            </div>
+            <button type="submit">Send</button>
+          </form>
+        </section>
+      </main>
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
---
+++ src/util/data.js
@@ -0,0 +1,74 @@
+export default [
+  {
+    "id": 1,
+    "name": "Curey",
+    "main-image": require("../images/CATazine1.png"),
+    "images": [],
+    "details": "Using Curey, you can instantly request a doctor to your home or reserve your turn at a clinic without leaving your doorsteps, Curey also provides access to hundreds of pharmacies, where you can order almost any drug",
+    "link": "https://curey-front-end.herokuapp.com/"
+  },
+  {
+    "id": 2,
+    "name": "Git clone OLX",
+    "main-image": require("../images/CATazine1.png"),
+    "images": [],
+    "details": "it's a website to sell used items to other people, similar to eBay or OLX - if you're arab -",
+    "link": "https://git-clone-olx.netlify.app"
+  },
+  {
+    "id": 4,
+    "name": "CAT Scope 15",
+    "main-image": require("../images/Scope15.png"),
+    "images": [require("../images/Scope15.png")],
+    "details": "It’s a page for Scope event which held for CAT Reloaded team in Faculty of Engineering Mansoura University",
+    "link": "https://bit.ly/2Kn4gr8"
+  },
+  {
+    "id": 5,
+    "name": "CAT Scope 16",
+    "main-image": require("../images/Scope 16.png"),
+    "images": [require("../images/Scope 16.png")],
+    "details": "It’s a page for Scope event which held for CAT Reloaded team in Faculty of Engineering Mansoura University",
+    "link": "https://bit.ly/2zhrywc"
+  },
+  {
+    "id": 6,
+    "name": "Sudoko app",
+    "main-image": require("../images/Sudoku.jpg"),
+    "images": [require("../images/Sudoku.jpg")],
+    "details": "It’s a simulation for sudoku game by pure js except game generator which I used a library to generate",
+    "link": "sudoku-sim.netlify.app"
+  },
+  {
+    "id": 7,
+    "name": "Catazine",
+    "main-image": require("../images/CATazine1.png"),
+    "images": [
+      require("../images/CATazine1.png"),
+      require("../images/CATazine2.png"),
+      require("../images/CATazine3.png"),
+      require("../images/CATazine4.png"),
+      require("../images/CATazine5.png"),
+      require("../images/CATazine6.png"),
+      require("../images/CATazine7.png")
+    ],
+    "details": "It's a magazine for CAT Reloaded team in Faculty of Engineering Mansoura University where student can read technical articles from CAT members and comment on this articles",
+    "link": ""
+  },
+  {
+    "id": 8,
+    "name": "Datetime picker",
+    "main-image": "",
+    "images": [],
+    "details": "It's a simple date time picker which allows user to select future date and type its time from 0 to 12 AM or PM",
+    "link": ""
+  },
+  {
+    "id": 3,
+    "name": "Calculator",
+    "main-image": require("../images/Calculator.png"),
+    "images": [require("../images/Calculator.png")],
+    "details": "It’s a simple simulation for calculator which can do simple operations like add, multiply, sin, cos, etc...",
+    "link": "https://bit.ly/2KmpmG0"
+  },
+]
