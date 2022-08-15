c255c8bf89467b30b20600dd8d8bc6d69a5b7df7
NahlaGalal <nahlaglal@gmail.com>
2022-08-15 04:38:30 +0200

    Add add project controller

--- src/controllers/projects.ts
+++ src/controllers/projects.ts
@@ -1,13 +1,16 @@
 import { Request, Response } from "express";
-import Project, { IProjectsDoc } from "../models/Projects";
-import Skill, { ISkillsDoc } from "../models/Skills";
+import Project from "../models/Projects";
+import Skill from "../models/Skills";
+import { IProjectsDoc } from "../types";
 
+// Get all projects
 export const getAllProjects = (req: Request, res: Response) => {
   Project.find({}, "name main_image backcolor")
     .sort({ end_date: -1 })
     .then((doc) => res.json([...doc]));
 };
 
+// Get project
 export const getProject = (req: Request, res: Response) => {
   const { id } = req.query;
   let project: IProjectsDoc;
@@ -21,4 +24,37 @@ export const getProject = (req: Request, res: Response) => {
       return res.json({ project });
     });
   });
+};
+
+// Add a project
+export const addProject = (req: Request, res: Response) => {
+  const projectData = req.body;
+  const { skills_images, main_image, images } = req.files as {
+    [fieldname: string]: Express.Multer.File[];
+  };
+
+  (async () => {
+    let promises = skills_images.map(async (skill) => {
+      const name = skill.originalname.slice(0, -4);
+      let res = await Skill.findOne({ name });
+      if (res) return res._id;
+      else {
+        let newSkill = await new Skill({
+          name,
+          image: skill.path,
+        }).save();
+        return newSkill._id;
+      }
+    });
+    const skillsIds = await Promise.all(promises);
+
+    await new Project({
+      ...projectData,
+      skills: skillsIds,
+      languages: JSON.parse(projectData.languages),
+      main_image: main_image[0].path,
+      images: images.map((img) => img.path),
+    }).save();
+    res.json({ done: true });
+  })();
 };
