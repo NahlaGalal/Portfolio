279b3ca10d8e9633844ae0e25f297d36e5c116c8
NahlaGalal <nahlaglal@gmail.com>
2022-08-15 04:15:17 +0200

    Add get project controller

--- src/controllers/projects.ts
+++ src/controllers/projects.ts
@@ -1,8 +1,24 @@
 import { Request, Response } from "express";
-import Project from "../models/Projects";
+import Project, { IProjectsDoc } from "../models/Projects";
+import Skill, { ISkillsDoc } from "../models/Skills";
 
 export const getAllProjects = (req: Request, res: Response) => {
   Project.find({}, "name main_image backcolor")
     .sort({ end_date: -1 })
     .then((doc) => res.json([...doc]));
+};
+
+export const getProject = (req: Request, res: Response) => {
+  const { id } = req.query;
+  let project: IProjectsDoc;
+
+  Project.findById(id).then((doc) => {
+    if (!doc) return res.json({ error: "No project" });
+    project = doc;
+
+    return Skill.find({ _id: { $in: [...doc.skills] } }).then((skills) => {
+      project.skills = skills;
+      return res.json({ project });
+    });
+  });
 };
