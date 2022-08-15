d63e2abe914e617521e62637c3d00eac60413eda
NahlaGalal <nahlaglal@gmail.com>
2022-08-15 05:43:20 +0200

    Add about component

---
+++ client/src/components/About/index.tsx
@@ -0,0 +1,106 @@
+import { BsTelephone, BsCloudDownload } from "react-icons/bs";
+import { HiOutlineMail, HiOutlineLocationMarker } from "react-icons/hi";
+import { FaHackerrank } from "react-icons/fa";
+import profile from "../../images/profile.jpg";
+import { Link } from "react-router-dom";
+
+const About = () => {
+  return (
+    <section className="[ flex flex-col justify-center items-center sm:flex-row-reverse sm:gap-x-20 ]">
+      {/* Avatar */}
+      <div
+        className="
+        [ w-72 h-64 sm:w-full sm:h-[100px] lg:h-[553px] lg:min-w-[530px] lg:w-[530px] ] 
+        [ overflow-visible rounded-3xl ] 
+        [ p-4 mx-auto sm:m-0 sm:p-8 lg:p-10 ] 
+        [ transition-all duration-500 ] 
+        [ bg-darkGrey dark:bg-lightGrey ]"
+      >
+        <img
+          src={profile}
+          alt="Nahla personal avatar"
+          className="[ h-64 w-64 sm:w-full sm:h-[400px] lg:w-[450px] lg:h-[553px] ] object-cover rounded-3xl"
+        />
+      </div>
+
+      {/* Info */}
+      <section>
+        <h1 className="text-5xl font-bold [ text-darkGreen dark:text-lightGreen ]">
+          Hello,
+        </h1>
+        <h2 className="my-4 text-3xl font-bold [ text-darkGreen dark:text-lightGreen ]">
+          I'm Nahla Galal
+        </h2>
+        <p className="leading-6 [ text-darkGrey dark:text-lightGrey ]">
+          Computer Science graduate passionate about Front-end. Highly-capable
+          leader, Leading committees at my faculty, Leading front-end at my
+          graduation project. Like to organize events, multitasking and
+          learning, Proficient in HTML, CSS.
+        </p>
+        <div className="[ grid grid-cols-[32px_1fr] gap-2 items-center ] mt-10">
+          <span className="rounded-full bg-darkGreen h-8 [ flex justify-center items-center ]">
+            <BsTelephone size={16} color="#EEEEEE" title="Phone" />
+          </span>
+          <p
+            title="Phone"
+            className="leading-6 [ text-darkGrey dark:text-lightGrey ]"
+          >
+            01097429922
+          </p>
+          <span className="rounded-full bg-darkGreen h-8 [ flex justify-center items-center ]">
+            <HiOutlineMail size={16} color="#EEEEEE" title="Email" />
+          </span>
+          <a
+            href="mailto:nahlaglal@gmail.com"
+            title="Email"
+            className="leading-6 [ text-darkGrey dark:text-lightGrey ]"
+          >
+            nahlaglal@gmail.com
+          </a>
+          <span className="rounded-full bg-darkGreen h-8 [ flex justify-center items-center ]">
+            <HiOutlineLocationMarker
+              size={16}
+              color="#EEEEEE"
+              title="Address"
+            />
+          </span>
+          <p
+            title="Address"
+            className="leading-6 [ text-darkGrey dark:text-lightGrey ]"
+          >
+            Cairo, Egypt
+          </p>
+          <span className="rounded-full bg-darkGreen h-8 [ flex justify-center items-center ]">
+            <FaHackerrank
+              size={16}
+              color="#EEEEEE"
+              title="Hacker rank account"
+            />
+          </span>
+          <a
+            href="https://www.hackerrank.com/nahlaglal?hr_r=1"
+            title="Hackerrank profile"
+            className="leading-6 [ text-darkGrey dark:text-lightGrey ]"
+          >
+            @nahlaglal
+          </a>
+        </div>
+        <Link
+          to="Nahla_Resume.pdf"
+          target="_blank"
+          download
+          className="
+            btn font-medium w-max 
+            [ flex items-center gap-2 ] 
+            [ px-4 ml-auto mt-8 ] 
+            [ bg-darkGreen dark:bg-lightGreen ]"
+        >
+          <BsCloudDownload size={24} color="#EEEEEE" />
+          Download CV
+        </Link>
+      </section>
+    </section>
+  );
+};
+
+export default About;
--- client/src/containers/Home.tsx
+++ client/src/containers/Home.tsx
@@ -1,16 +1,31 @@
-import React from 'react'
-import Sidebar from '../components/Sidebar'
+import React, { useState } from "react";
+import About from "../components/About";
+import Sidebar from "../components/Sidebar";
 
 const Home = () => {
-  const onChangeSecHandler = (section: string) => {
+  const [section, setSection] = useState<string>("Home");
 
-  }
+  const onChangeSecHandler = (sec: string) => setSection(sec);
+
+  const getCurrentSection = () => {
+    switch (section) {
+      case "Home":
+        return <About />;
+      default:
+        return <></>;
+    }
+  };
 
   return (
     <>
-      <Sidebar onClick={onChangeSecHandler}/>
+      <Sidebar onClick={onChangeSecHandler} />
+      <main
+        className="[ px-[5%] sm:px-[10%] my-10 ] [ min-h-[calc(100vh-80px)] ]"
+      >
+        {getCurrentSection()}
+      </main>
     </>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
