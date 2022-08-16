013503b66fcc82cc9ef8a8deda186ecb5c13a565
NahlaGalal <nahlaglal@gmail.com>
2022-08-17 01:35:34 +0200

    Add experience routes

--- src/routes.ts
+++ src/routes.ts
@@ -1,4 +1,5 @@
 import { Router } from "express";
+import { addExperience, getAllExperience } from "./controllers/experience";
 import { getAllProjects, getProject, addProject } from "./controllers/projects";
 
 const router = Router();
@@ -8,5 +9,9 @@ router.get("/api/", getAllProjects);
 router.get("/api/project", getProject);
 
 router.post("/api/project", addProject);
+
+router.get("/api/experience", getAllExperience);
+
+router.post("/api/experience", addExperience);
 
 export default router;
