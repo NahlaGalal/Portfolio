bb9f1e8f4a5b51f5365b26fe795dd92101d4d363
NahlaGalal <nahlaglal@gmail.com>
2022-08-17 02:17:01 +0200

    Split experience and education in get controller

--- src/controllers/experience.ts
+++ src/controllers/experience.ts
@@ -4,8 +4,13 @@ import Experience from "../models/Experience";
 // Get experience
 export const getAllExperience = (req: Request, res: Response) => {
   Experience.find()
-    .sort({ end_date: -1 })
-    .then((doc) => res.json([...doc]));
+    .sort({ end_date: -1, start_date: -1 })
+    .then((doc) => {
+      const experience = doc.filter((item) => item.type === "experience");
+      const education = doc.filter((item) => item.type === "education");
+
+      return res.json({ experience, education });
+    });
 };
 
 // Add experience / education
