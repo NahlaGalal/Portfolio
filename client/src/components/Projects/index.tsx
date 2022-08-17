24901f3192fc5cfd7a82dbd4da93c2d3ed68d089
NahlaGalal <nahlaglal@gmail.com>
2022-08-17 03:25:28 +0200

    Modify paging fucntionality in front end

--- client/src/components/Projects/index.tsx
+++ client/src/components/Projects/index.tsx
@@ -7,10 +7,18 @@ import { IProject } from "./Types";
 const Projects = () => {
   const [page, setPage] = useState<number>(1);
   const [projects, setProjects] = useState<IProject[]>([]);
+  const [pages, setPages] = useState<number>(0);
 
   useEffect(() => {
-    axios.get("api").then((res) => setProjects(res.data));
-  }, []);
+    axios.get("api").then((res) => {
+      setProjects(res.data.data);
+      setPages(res.data.pages);
+    });
+  }, [setProjects]);
+
+  useEffect(() => {
+    axios.get(`api?page=${page}`).then((res) => setProjects(res.data.data));
+  }, [page]);
 
   return (
     <section className="sm:grid sm:grid-cols-[repeat(3,_1fr)] sm:gap-x-10">
@@ -18,12 +26,12 @@ const Projects = () => {
       <span className="heading-border sm:col-[1/4]"></span>
 
       {/* Projects */}
-      {projects.slice((page - 1) * 3, page * 3).map((project) => (
+      {projects.map((project) => (
         <ProjectBox project={project} key={project._id} />
       ))}
 
       {/* Paging */}
-      <Paging pagesNum={projects.length / 3} page={page} setPage={setPage} />
+      <Paging pagesNum={pages} page={page} setPage={setPage} />
     </section>
   );
 };
