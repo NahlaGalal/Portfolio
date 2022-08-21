87414b6b1f99015cbfc07c6ecc3a2c74385d9d49
NahlaGalal <nahlaglal@gmail.com>
2022-08-21 04:02:00 +0200

    Add get skills controller

--- src/controllers/experience.ts
+++ src/controllers/experience.ts
@@ -1,15 +1,28 @@
 import { Request, Response } from "express";
+import { Types } from "mongoose";
 import Experience from "../models/Experience";
+import Skills from "../models/Skills";
+import { IExperienceDoc } from "../types";
 
 // Get experience
 export const getAllExperience = (req: Request, res: Response) => {
+  let experience: (IExperienceDoc & {
+      _id: Types.ObjectId;
+    })[],
+    education: (IExperienceDoc & {
+      _id: Types.ObjectId;
+    })[];
+
   Experience.find()
     .sort({ end_date: -1, start_date: -1 })
     .then((doc) => {
-      const experience = doc.filter((item) => item.type === "experience");
-      const education = doc.filter((item) => item.type === "education");
+      experience = doc.filter((item) => item.type === "experience");
+      education = doc.filter((item) => item.type === "education");
 
-      return res.json({ experience, education });
+      return Skills.find({}, "name");
+    })
+    .then((skills) => {
+      return res.json({ experience, education, skills });
     });
 };
 
